de8570bd38d25b260f7447c6bbec92b3978ae25a
dmiropol <16676640+dmiropol@users.noreply.github.com>
2022-02-17 10:46:31 -0500

    moved ncp cleanup script

--- destroy-cluster.sh
+++ destroy-cluster.sh
@@ -11,10 +11,16 @@ echo "###################### Started Destruction of the K8s cluster ############
 cd kube-cluster
 echo "################################# Deleting load balancer... #################################"
 ansible-playbook -i $hosts_file delete-loadbalancer.yaml
-echo "###################### Deleting network addons... #################################"
-ansible-playbook -i $hosts_file delete-network-addons.yaml
+if [ "$1" == 'antrea' ]; then 
+    echo "################################# Destroying Antrea-NSX interworking... #################################"
+    ansible-playbook -i $hosts_file delete-network-addons.yaml
+fi
 echo "###################### Deleting cluster network... #################################"
 ansible-playbook -i $hosts_file delete-network.yaml
 echo "################################# Reseting cluster and cleaning up files... #################################"
 ansible-playbook -i $hosts_file reset-cluster.yaml
 echo "################################# Done Destroying K8s cluster #################################"
+if [ "$1" == 'ncp' ]; then 
+    echo "################################# Cleaning up NCP objects in NSX...  #################################"
+    ~/nfs_share/Projects/ncp/cleanup-ncp.sh
+fi
